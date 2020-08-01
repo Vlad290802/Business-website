ce5a7f364f4ad15960f02400ea1fb5d46be0edf6
Vlad <dvlad4503@gmail.com>
2020-08-01 21:09:34 +0300

    fifth commit

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,87 @@ $(document).ready(function(){
       nextArrow: '<button type="button" class="slick-next"><img src="../icons/right-solid.png"></button>'
     });
   });
+
+//Smooth and scroll up
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 1000){
+      $('.pageup').fadeIn();
+    }else{
+      $('.pageup').fadeOut();
+    }
+  });
+  $("a[href^=#up]").click(function(){
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 1000){
+      $('promo__link').fadeIn();
+    }else{
+      $('promo__link').fadeOut();
+    }
+  });
+  $("a[href^=#promo]").click(function(){
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+  $("a[href^=#module]").click(function(){
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+  $("a[href^=#brunch]").click(function(){
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+  $("a[href^=#advantages]").click(function(){
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+
+
+      // Tabs
+// document.addEventListener('DOMContentLoaded', () => {
+
+//     const tabs = document.querySelectorAll('.brunch__tab'),
+//           tabsCo = document.querySelector('.brunch__inner'),
+//           tabsContent = tabsCo.querySelectorAll('.brunch__content'),
+//           tabsParent = document.querySelector('.brunch__tabs');
+
+//     function hideTabContent() {
+//         tabsContent.forEach(item => {
+//             item.style.display = 'none';
+//         });
+
+//         tabs.forEach(item => {
+//             item.classList.remove('brunch__tab_active');
+//         });
+//     }
+
+//     function showTabContent(i = 0) {
+//         tabsContent[i].style.display = 'block';
+//         tabs[i].classList.add('tabheader__item_active');
+//     }
+
+//     hideTabContent();
+//     showTabContent();
+
+//     tabsParent.addEventListener('click', (event) => {
+//         const target = event.target;
+        
+// 		if(target && target.classList.contains('brunch__tab')) {
+//             tabs.forEach((item, i) => {
+//                 if (target == item) {
+//                     hideTabContent();
+//                     showTabContent(i);
+//                 }
+//             });
+// 		}
+// 	});
+// });
