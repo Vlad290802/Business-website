f623ced4821f0bcc69ebb42bbe2671fadfc6c067
Vlad <dvlad4503@gmail.com>
2020-08-06 21:31:31 +0300

    eights commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 		$('.promo__carousel').slick({
-			dots: true,
+			dots: false,
 			speed: 900,
 			fade: true,
 			cssEase: 'linear',
@@ -117,6 +117,27 @@ $(document).ready(function(){
         }
         window.addEventListener('scroll', showModalByScroll);
 });
+
+
+//hamburger
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+});
+
 
 
 	 // Tabs
